bae70c8e52e05e481486253a6b6bc0a6c7d1f737
cmrivera <cmrivera0004@student.stcc.edu>
2020-08-25 20:59:30 -0400

    queries almost done

--- queries.sql
+++ queries.sql
@@ -2,8 +2,25 @@
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
 
+SELECT c.CategoryName AS "Category", p.ProductName
+FROM "Product" AS p
+  JOIN "Category" AS c ON p.CategoryId = c.Id
+
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
 
+SELECT o.Id, s.CompanyName
+FROM "Order" AS o
+  JOIN "Shipper" AS s
+  ON o.ShipVia = s.Id
+WHERE o.OrderDate < '2012-08-09'
+
+
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
+
+SELECT p.ProductName, d.quantity
+FROM "Product" AS p
+  JOIN OrderDetail AS d
+  ON p.Id = d.ProductId
+WHERE d.OrderId = 10251 
 
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
