6cf9b247e7f074e8512168310113e33e8277ab53
cmrivera <cmrivera0004@student.stcc.edu>
2020-08-25 21:05:19 -0400

    working on scheme-model

---
+++ schemes/scheme-model.js
@@ -0,0 +1,43 @@
+const db = require("../data/db-config");
+
+module.exports = {
+  find,
+  findById,
+  add,
+  findSteps,
+  update,
+  remove,
+};
+
+function find() {
+  return db("schemes");
+}
+
+function findById(id) {
+  return db("schemes").where({ id }).first();
+}
+
+function add(schemeData) {
+  return db("schemes").insert(schemeData);
+}
+
+function findSteps(id) {
+  return db("steps as s")
+    .join("schemes as k", "k.id", "s.scheme_id")
+    .select("s.id", "k.scheme_name", "s.step_number", "s.instructions")
+    .orderBy("s.step_number")
+    .where({ scheme_id: id });
+}
+
+function update(changes, id) {
+  return db("schemes")
+    .where({ id })
+    .update(changes)
+    .then((count) => {
+      return findById(id);
+    });
+}
+
+function remove(id) {
+  return db("schemes").where({ id }).del();
+}
